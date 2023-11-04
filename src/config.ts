30001aeb9ae13b922805320358cee85f7ebd7df6
Davis Sawali <davis.sawali@ecadlabs.com>
2023-11-04 14:12:53 -0700

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const env = {
   rpc: import.meta.env.VITE_RPC_URL|| 'https://ghostnet.ecadinfra.com',
-  contractAddress: import.meta.env.VITE_CONTRACT_ADDRESS || '',
+  contractAddress: import.meta.env.VITE_CONTRACT_ADDRESS || 'KT1JwZD1y1YvJyoNyB4xxc1sMEawMDXf7GcU',
 }
